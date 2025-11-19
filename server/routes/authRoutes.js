e09e3ae7a3b188795e6aa19121de0901a812ed78
INWOSU123 <108906703+INWOSU123@users.noreply.github.com>
2025-11-18 21:00:10 -0600

    Updated authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signup, login, logout, getMe } from "../controllers/authControllers.js";
+import { signup, login, logout, getMe, forgotPassword } from "../controllers/authControllers.js";
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.post("/forgot-password",forgotPassword);
 
 // 2 key information from here => Route is api/auth/myInfo request is a GET request
 router.get("/myInfo", getMe);
