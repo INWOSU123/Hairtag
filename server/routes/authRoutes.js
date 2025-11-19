53994e540f22d9f42c49afa4d98217c24dc1b692
INWOSU123 <108906703+INWOSU123@users.noreply.github.com>
2025-11-18 21:13:04 -0600

    Added resetpassword

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { signup, login, logout, getMe, forgotPassword } from "../controllers/authControllers.js";
+import { signup, login, logout, getMe, forgotPassword,resetPassword } from "../controllers/authControllers.js";
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/forgot-password",forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 // 2 key information from here => Route is api/auth/myInfo request is a GET request
 router.get("/myInfo", getMe);
